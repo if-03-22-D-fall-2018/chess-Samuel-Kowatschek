e03ba1fcf86b4324364bfeaafc5ef4a7d0f6a2f9
Samuel Kowatschek <samkow@gmx.at>
2018-10-25 13:35:15 +0200

    added couple of things to chess.h

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,3 +10,26 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+
+enum PieceType{
+    Pawn, Knight, Rook, Bishop, Queen, King
+};
+
+
+enum Color{White,Black};
+
+struct ChessPiece{
+    enum Color color;
+    enum PieceType piece;
+};
+
+struct ChessSquare{
+  bool is_set;
+  struct ChessPiece piece;
+};
+
+typedef ChessSquare ChessBoard[8][8] ;
+
+bool is_piece(ChessBoard board,enum Color color,enum PieceType type);
+
+void init_chess_board(ChessBoard chess_board);
