5ac90e3a99c92a4b9f9b8157db1d051d4c7c6c37
Kowatschek Samuel <samuel@saintvis.com>
2018-10-28 18:37:50 +0100

    Fixed a lot of errors

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,6 +10,8 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+#include <stdbool.h>
+
 
 enum PieceType{
     Pawn, Knight, Rook, Bishop, Queen, King
@@ -21,15 +23,24 @@ enum Color{White,Black};
 struct ChessPiece{
     enum Color color;
     enum PieceType piece;
+
+};
+
+struct NoPiece{
+  enum Color color;
+  enum PieceType piece;
 };
 
 struct ChessSquare{
   bool is_set;
   struct ChessPiece piece;
+  bool is_occupied;
 };
 
 typedef ChessSquare ChessBoard[8][8] ;
-
-bool is_piece(ChessBoard board,enum Color color,enum PieceType type);
-
+bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type);
 void init_chess_board(ChessBoard chess_board);
+struct ChessSquare* get_square(ChessBoard chess_board,  char file, int rank);
+bool is_square_occupied(ChessBoard chess_board, char file, int rank);
+bool add_piece(ChessBoard chess_board, char file, int rank, struct ChessPiece chess_piece);
+struct ChessPiece get_piece(ChessBoard chess_board, char file, int rank);
