6a2d7745a89fecd1b72861f7f00a47d2b16065c9
Kowatschek Samuel <samuel@saintvis.com>
2018-10-28 21:30:22 +0100

    Fixed lots of errors, only NoPiece NormalMove and CaptureMove aren't working

--- assignment/chess.h
+++ assignment/chess.h
@@ -22,13 +22,8 @@ enum Color{White,Black};
 
 struct ChessPiece{
     enum Color color;
-    enum PieceType piece;
+    enum PieceType type;
 
-};
-
-struct NoPiece{
-  enum Color color;
-  enum PieceType piece;
 };
 
 struct ChessSquare{
@@ -38,9 +33,22 @@ struct ChessSquare{
 };
 
 typedef ChessSquare ChessBoard[8][8] ;
+typedef char File;
+typedef int Rank;
+
 bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type);
 void init_chess_board(ChessBoard chess_board);
 struct ChessSquare* get_square(ChessBoard chess_board,  char file, int rank);
 bool is_square_occupied(ChessBoard chess_board, char file, int rank);
 bool add_piece(ChessBoard chess_board, char file, int rank, struct ChessPiece chess_piece);
 struct ChessPiece get_piece(ChessBoard chess_board, char file, int rank);
+void setup_chess_board(ChessBoard chess_board);
+bool remove_piece(ChessBoard chess_board, File file, Rank rank);
+
+bool squares_share_file(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_rank(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_diagonal(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_knights_move(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_pawns_move(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_queens_move(File file1,Rank rank1,File file2, Rank rank2);
+bool squares_share_kings_move(File file1,Rank rank1,File file2, Rank rank2);
